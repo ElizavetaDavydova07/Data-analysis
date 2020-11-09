09c60dd8d546c14aa96a10374876af9f800cbc18
ElizavetaDavydova07 <71983263+ElizavetaDavydova07@users.noreply.github.com>
2020-11-09 12:35:46 +0300

    'SELECT <data> INTO movie.top_rated_tags' added

--- home_work_4/hw4.sql
+++ home_work_4/hw4.sql
@@ -8,7 +8,8 @@ CREATE TABLE content_genres ( movieid int, genre text );
 
 -- transform our data 
 WITH top_rated AS 
-(SELECT movieid, AVG(rating) as avg_rating FROM movie.ratings GROUP BY movieid HAVING COUNT(*) > 50 ORDER BY movieid, avg_rating  DESC LIMIT 150) SELECT top_rated.movieid, top_rated.avg_rating, genres.keywords INTO top_rated_tags FROM top_rated 
+(SELECT movieid, AVG(rating) as avg_rating FROM movie.ratings GROUP BY movieid HAVING COUNT(*) > 50 ORDER BY movieid, avg_rating  DESC LIMIT 150)
+SELECT top_rated.movieid, top_rated.avg_rating, genres.keywords INTO movie.top_rated_tags FROM top_rated 
 INNER JOIN 
 (SELECT public.content_genres.movieid, array_agg(genre) as keywords FROM public.content_genres GROUP BY public.content_genres.movieid) AS genres 
 ON genres.movieid = top_rated.movieid;

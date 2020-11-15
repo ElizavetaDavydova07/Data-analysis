d3c6dd9b91ec925366def2e94cc839d92da8eeb7
ElizavetaDavydova07 <71983263+ElizavetaDavydova07@users.noreply.github.com>
2020-11-15 15:34:34 +0300

    agg.js added

---
+++ home_work_5/agg.js
@@ -0,0 +1,24 @@
+/*
+       Вывести общее количество документoв в коллекции 
+*/
+tags_count = db.tags.count()
+print('tags count: ', tags_count);
+/*
+        Вывести количество тэгов Adventure в коллекции
+*/
+adv_count = db.tags.find({"tag_name" : "Adventure"}).count()
+print('Adventure" tags count: ', adv_count);
+/*
+        Вывести топ 3 самых распространенных тэгов
+*/
+printjson(
+        db.tags.aggregate([
+                {"$group": {
+                                "_id": "$tag_name",
+                                "tag_count": "{ $sum: 1 }"
+                           }
+                },
+                {$sort : {tag_count : -1 }},
+                {$limit: 3}
+        ])['_batch']
+);

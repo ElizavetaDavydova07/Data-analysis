e94740305276b1840e40c95bad93e6fd99108168
ElizavetaDavydova07 <71983263+ElizavetaDavydova07@users.noreply.github.com>
2020-11-16 10:41:04 +0300

    Update agg.js

--- home_work_5/agg.js
+++ home_work_5/agg.js
@@ -18,7 +18,7 @@ printjson(
                                 "tag_count": { "$sum": 1 }
                            }
                 },
-                {"$sort": {tag_count : -1 }},
+                {"$sort": {"tag_count" : -1 }},
                 {"limit" 3}
         ])['_batch']
 );
